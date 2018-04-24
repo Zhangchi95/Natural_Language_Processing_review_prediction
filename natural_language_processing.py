a4ed1973de17a6a953f1b51e4625625c8050c8b2
Zhangchi <32311711+Zhangchi95@users.noreply.github.com>
2018-04-24 00:22:20 -0400

    Add files via upload

---
+++ natural_language_processing.py
@@ -0,0 +1,66 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+def WordPreProcess(words):
+    review = words.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    return review
+    
+corpus = []
+
+for i in range(0, 1000):
+   review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+   corpus.append(WordPreProcess(review))
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Linear SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# For user input their own review
+import sys
+inputreview = str(sys.argv[1])
+myre = [WordPreProcess(inputreview)]
+myreview = cv.transform(myre).toarray()
+rate = classifier.predict(myreview)
+def PrintResult(rate):
+    if (rate == 1):
+        print "Positive"
+    else : print "Negetive"
+PrintResult(rate)
+
+    
